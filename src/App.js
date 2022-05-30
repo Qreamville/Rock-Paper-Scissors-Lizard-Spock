99c9756a129d8d7e3db43bd848fe868450ae0c72
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-30 00:51:15 -1200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Modal } from "./components";
+import { HashRouter } from "react-router-dom";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    const scoreItem = JSON.parse(localStorage.getItem("score"));
+    if (scoreItem) {
+      setScore(scoreItem);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("score", JSON.stringify(score));
+  }, [score]);
+
   return (
-    <div className="App">
-      <Modal />
-    </div>
+    <HashRouter>
+      <div className="App flex justify-start flex-col items-center">
+        {modal && <Modal setModal={setModal} />}
+      </div>
+    </HashRouter>
   );
 }
 
