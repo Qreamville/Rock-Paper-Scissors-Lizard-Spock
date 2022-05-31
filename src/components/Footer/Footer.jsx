3e70d29cca91d15e01620850d123e44397e4edd4
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-30 23:14:37 -1200

    Complete footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="absolute bottom-0">
+      <p className="text-white">&copy;Copyright Qreamville 2022</p>
+    </footer>
+  );
 };
 
 export default Footer;
