71fab353b6a17d6fb5594558551eb6f44410c163
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-30 22:54:59 -1200

    Complete  rules btn function

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <HashRouter>
       <div className="App flex justify-start flex-col items-center">
-        <Header score={score} />
+        <Header score={score} setModal={setModal} />
         {modal && <Modal setModal={setModal} />}
       </div>
     </HashRouter>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./header.css";
 
-const Header = ({ score }) => {
+const Header = ({ score, setModal }) => {
   return (
-    <header className="w-full md:w-3/4 lg:w-2/5">
+    <header className="w-full md:w-3/4 lg:w-2/5 flex flex-col">
       <div className="header__main flex flex-row justify-between items-center px-2 py-1">
         <ul className="font-semibold text-white text-lg">
           <li>ROCK</li>
@@ -16,6 +16,14 @@ const Header = ({ score }) => {
           <p className="font-bold">SCORE</p>
           <h1 className="font-bold text-4xl">{score}</h1>
         </div>
+      </div>
+      <div className="header-btn text-center mt-4">
+        <button
+          className="text-white px-6 py-1 rounded-md font-semibold"
+          onClick={() => setModal(true)}
+        >
+          Rules
+        </button>
       </div>
     </header>
   );
